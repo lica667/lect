d72b95e1b8b6e43762e34eeb63ea7df2ce7e1c71
Tanya <lica667@gmail.com>
2014-10-06 00:32:45 +0300

    add deivse to lecturer add table student

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141005195800) do
+ActiveRecord::Schema.define(version: 20141005213121) do
 
   create_table "lecturers", force: true do |t|
     t.string   "name"
     t.string   "sublects"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+  end
+
+  add_index "lecturers", ["email"], name: "index_lecturers_on_email", unique: true
+  add_index "lecturers", ["reset_password_token"], name: "index_lecturers_on_reset_password_token", unique: true
+
+  create_table "students", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
